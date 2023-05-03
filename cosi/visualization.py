d8611988fdf57e42f560e5923408f8c2257b7990
cosmos1030 <dyk6208@gmail.com>
2023-05-03 05:33:50 +0000

    first commit

--- cosi/visualization.py
+++ cosi/visualization.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import ScalarFormatter, FixedLocator
+import koreanize_matplotlib
 
 
 ScalarFormatter().set_scientific(False)
